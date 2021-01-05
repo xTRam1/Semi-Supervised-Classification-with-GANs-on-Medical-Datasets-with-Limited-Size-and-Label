42e72ec3a92774d6f0b46e0292a9e9e8fe0b7a48
Lütfi Eren Erdoğan <rosedragon1995@gmail.com>
2021-01-05 22:04:35 +0300

    Update test.py

--- SS-DiffAugment-GAN/test.py
+++ SS-DiffAugment-GAN/test.py
@@ -32,39 +32,15 @@ def main():
     logger = create_logger(args.path_helper['log_path'], phase='test')
 
     dis_net1 = Discriminator(args=args).cuda()
-    dis_net2 = Discriminator(args=args).cuda()
-    dis_net3 = Discriminator(args=args).cuda()
-    dis_net4 = Discriminator(args=args).cuda()
 
     # set writer
     print(f'=> resuming from {args.load_path}')
     assert os.path.exists(args.load_path)
-    checkpoint_file1 = os.path.join(args.load_path, 'Model', 'checkpoint_best1.pth')
-    checkpoint_file2 = os.path.join(args.load_path, 'Model', 'checkpoint_best2.pth')
-    checkpoint_file3 = os.path.join(args.load_path, 'Model', 'checkpoint_best3.pth')
-    checkpoint_file4 = os.path.join(args.load_path, 'Model', 'checkpoint_last.pth')
-    assert os.path.exists(checkpoint_file1)
-    assert os.path.exists(checkpoint_file2)
-    assert os.path.exists(checkpoint_file3)
-    assert os.path.exists(checkpoint_file4)
-    checkpoint1 = torch.load(checkpoint_file1)
-    checkpoint2 = torch.load(checkpoint_file2)
-    checkpoint3 = torch.load(checkpoint_file3)
-    checkpoint4 = torch.load(checkpoint_file4)
-    
+    checkpoint1 = torch.load(args.load_path)
     print(checkpoint1['epoch'] + 1)
-    print(checkpoint2['epoch'] + 1)
-    print(checkpoint3['epoch'] + 1)
-    print(checkpoint4['epoch'] + 1)
     
     dis_net1.load_state_dict(checkpoint1['dis_state_dict'])
     logger.info(f'=> loaded checkpoint {checkpoint_file1}')
-    dis_net2.load_state_dict(checkpoint2['dis_state_dict'])
-    logger.info(f'=> loaded checkpoint {checkpoint_file2}')
-    dis_net3.load_state_dict(checkpoint3['dis_state_dict'])
-    logger.info(f'=> loaded checkpoint {checkpoint_file3}')
-    dis_net4.load_state_dict(checkpoint3['dis_state_dict'])
-    logger.info(f'=> loaded checkpoint {checkpoint_file4}')
     
     logger.info(args)
     writer_dict = {
@@ -79,19 +55,8 @@ def main():
 
     final_val_acc1 = get_val_acc(val_loader, dis_net1)
     final_test_acc1 = get_val_acc(test_loader, dis_net1)
-    final_val_acc2 = get_val_acc(val_loader, dis_net2)
-    final_test_acc2 = get_val_acc(test_loader, dis_net2)
-    final_val_acc3 = get_val_acc(val_loader, dis_net3)
-    final_test_acc3 = get_val_acc(test_loader, dis_net3)
-    final_val_acc4 = get_val_acc(val_loader, dis_net4)
-    final_test_acc4 = get_val_acc(test_loader, dis_net4)
-    final_test_acc = (final_test_acc1.data + final_test_acc2.data + final_test_acc3.data) / 3
 
-    print('\n ----- \n 1. Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc1.data, final_test_acc1.data))
-    print('\n ----- \n 2. Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc2.data, final_test_acc2.data))
-    print('\n ----- \n 3. Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc3.data, final_test_acc3.data))
-    print('\n ----- \n Last Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc4.data, final_test_acc4.data))
-    print('\n ----- \n Final Test Acc:', final_test_acc)
+    print('\n ----- \n Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc1.data, final_test_acc1.data))
 
 if __name__ == '__main__':
     main()
