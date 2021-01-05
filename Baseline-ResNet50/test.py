2e9d5e76c12a4ec3a2aaf94a5c1c015dc146b305
Lütfi Eren Erdoğan <rosedragon1995@gmail.com>
2021-01-05 22:15:53 +0300

    Update test.py

--- Baseline-ResNet50/test.py
+++ Baseline-ResNet50/test.py
@@ -26,30 +26,13 @@ def main():
     logger = create_logger(args.path_helper['log_path'], phase='test')
 
     resnet1 = torchvision.models.resnet50(pretrained=False, progress=False)
-    resnet2 = torchvision.models.resnet50(pretrained=False, progress=False)
-    resnet3 = torchvision.models.resnet50(pretrained=False, progress=False)
-    resnet4 = torchvision.models.resnet50(pretrained=False, progress=False)
     
     # set writer
     print(f'=> resuming from {args.load_path}')
     assert os.path.exists(args.load_path)
-    checkpoint_file1 = os.path.join(args.load_path, 'Model', 'checkpoint_best1.pth')
-    checkpoint_file2 = os.path.join(args.load_path, 'Model', 'checkpoint_best2.pth')
-    checkpoint_file3 = os.path.join(args.load_path, 'Model', 'checkpoint_best3.pth')
-    checkpoint_file4 = os.path.join(args.load_path, 'Model', 'checkpoint_last.pth')
-    assert os.path.exists(checkpoint_file1)
-    assert os.path.exists(checkpoint_file2)
-    assert os.path.exists(checkpoint_file3)
-    assert os.path.exists(checkpoint_file4)
-    checkpoint1 = torch.load(checkpoint_file1)
-    checkpoint2 = torch.load(checkpoint_file2)
-    checkpoint3 = torch.load(checkpoint_file3)
-    checkpoint4 = torch.load(checkpoint_file4)
+    checkpoint1 = torch.load(args.load_path)
     resnet1.load_state_dict(checkpoint1['resnet_state_dict'])
-    resnet2.load_state_dict(checkpoint2['resnet_state_dict'])
-    resnet3.load_state_dict(checkpoint3['resnet_state_dict'])
-    resnet4.load_state_dict(checkpoint4['resnet_state_dict'])
-    logger.info(f'=> loaded checkpoint {checkpoint_file}')
+    logger.info(f'=> loaded checkpoint {args.load_path}')
 
     logger.info(args)
     writer_dict = {
@@ -63,21 +46,8 @@ def main():
 
     final_val_acc1 = get_val_acc(val_loader, resnet1)
     final_test_acc1 = get_val_acc(test_loader, resnet1)
-    final_val_acc2 = get_val_acc(val_loader, resnet2)
-    final_test_acc2 = get_val_acc(test_loader, resnet2)
-    final_val_acc3 = get_val_acc(val_loader, resnet3)
-    final_test_acc3 = get_val_acc(test_loader, resnet3)
-    final_val_acc4 = get_val_acc(val_loader, resnet4)
-    final_test_acc4 = get_val_acc(test_loader, resnet4)
-    
-    final_test_acc = (final_test_acc1.data + final_test_acc2.data + final_test_acc3.data) / 3
 
-    print('\n ----- \n 1. Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc1.data, final_test_acc1.data))
-    print('\n ----- \n 2. Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc2.data, final_test_acc2.data))
-    print('\n ----- \n 3. Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc3.data, final_test_acc3.data))
-    print('\n ----- \n Last Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc4.data, final_test_acc4.data))
-    print('\n ----- \n Final Test Acc:', final_test_acc)
-    
+    print('\n ----- \n Model \n Final Validation Accuracy: {0}, \n Final Test Accuracy: {1}'.format(final_val_acc1.data, final_test_acc1.data))
 
 if __name__ == '__main__':
     main()
